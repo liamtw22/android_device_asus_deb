e9d7453f012bc80366840a62b6ea02d85ec409fe
followmsi <followmsi@gmail.com>
2019-01-23 15:18:31 +0100

    RR-fy - Get Resurrected

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/lineage_deb.mk
+	$(LOCAL_DIR)/rr_deb.mk
--- rr_deb.mk
+++ rr_deb.mk
@@ -5,10 +5,10 @@ TARGET_SCREEN_WIDTH := 1200
 TARGET_BOOTANIMATION_HALF_RES := true
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/lineage/config/common_mini_tablet_wifionly.mk)
+$(call inherit-product, vendor/rr/config/common_mini_tablet_wifionly.mk)
 
 # Inherit telephony stuff
-$(call inherit-product, vendor/lineage/config/telephony.mk)
+$(call inherit-product, vendor/rr/config/telephony.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
@@ -19,7 +19,7 @@ $(call inherit-product-if-exists, vendor/asus/deb/device-vendor.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := deb
-PRODUCT_NAME := lineage_deb
+PRODUCT_NAME := rr_deb
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := asus
